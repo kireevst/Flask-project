f61ed9e96692b47f44d5ecf82991457982e071e0
kireevst <kireevstepan11@gmail.com>
2025-05-05 17:58:58 +0400

    самая базовая авторизация
    добавлена авторизация

---
+++ main.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, redirect
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
+
+from loginform import LoginForm
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'secret_key'
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return "Привет, Яндекс!"
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        return redirect('/success')
+    return render_template('login.html', title='Авторизация', form=form)
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
