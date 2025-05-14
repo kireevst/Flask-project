70231039eedd65c7d6c6d2319021105d58378abd
kireevst <kireevstepan11@gmail.com>
2025-05-14 16:00:14 +0400

    admin: START

--- data/news.py
+++ data/news.py
@@ -15,7 +15,9 @@ class News(SqlAlchemyBase):
     created_date = sqlalchemy.Column(sqlalchemy.DateTime,
                                      default=datetime.datetime.now)
     is_private = sqlalchemy.Column(sqlalchemy.Boolean, default=True)
-
+    is_checked = sqlalchemy.Column(sqlalchemy.Boolean, default=False)
+    is_banned = sqlalchemy.Column(sqlalchemy.Boolean, default=False)
+    approved = sqlalchemy.Column(sqlalchemy.Boolean, default=False)
     user_id = sqlalchemy.Column(sqlalchemy.Integer,
                                 sqlalchemy.ForeignKey("users.id"))
     # game_title = sqlalchemy.Column(sqlalchemy.String, nullable=True)
